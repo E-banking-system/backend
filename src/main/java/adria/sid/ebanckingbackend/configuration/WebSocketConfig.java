f78ed43088e9c21b484de5138a6ed7b3a9f3a750
kao19 <feiyukine@gmail.com>
2023-08-08 13:24:59 +0100

    chat controller

--- src/main/java/adria/sid/ebanckingbackend/configuration/WebSocketConfig.java
+++ src/main/java/adria/sid/ebanckingbackend/configuration/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic"); // Enables a simple in-memory message broker
+        config.enableSimpleBroker("/topic");
         config.setApplicationDestinationPrefixes("/app"); // Prefix for messages to be routed to @MessageMapping methods
     }
 
---
+++ src/main/java/adria/sid/ebanckingbackend/controllers/ChatController.java
@@ -0,0 +1,23 @@
+package adria.sid.ebanckingbackend.controllers;
+
+import adria.sid.ebanckingbackend.entities.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public Message sendMessage(@Payload Message message) {
+        // Save the message in the database
+        messageRepository.save(message);
+        return message;
+    }
+}
