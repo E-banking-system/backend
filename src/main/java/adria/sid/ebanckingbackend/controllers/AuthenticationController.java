576950e33d5769ba0fead7b1b1199d9eb3a19172
TAFFAHACHRAF <a.taffah@etu.enset-media.ac.ma>
2023-08-18 07:11:08 +0200

    changing class diagram

--- src/main/java/adria/sid/ebanckingbackend/controllers/AuthenticationController.java
+++ src/main/java/adria/sid/ebanckingbackend/controllers/AuthenticationController.java
@@ -12,7 +12,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
