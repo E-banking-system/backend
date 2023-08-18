d035267486b88e1df4914bfe76b1bd0e66785253
kao19 <feiyukine@gmail.com>
2023-08-18 14:07:31 +0100

    fix query

--- src/main/java/adria/sid/ebanckingbackend/repositories/MessageRepository.java
+++ src/main/java/adria/sid/ebanckingbackend/repositories/MessageRepository.java
@@ -2,11 +2,17 @@ package adria.sid.ebanckingbackend.repositories;
 
 import adria.sid.ebanckingbackend.entities.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, String> {
     List<Message> getMessagesBySenderIdOrReceiverId(String senderId,String receiverId);
-    List<Message> getMessagesBySenderIdAndReceiverId(String senderId,String receiverId);
+
+    @Query(value = "SELECT m FROM Message m " +
+            "WHERE (m.sender.id = :userId OR m.receiver.id = :userId) " +
+            "AND (m.receiver.id = :receiverId OR m.sender.id = :receiverId)")
+    List<Message> getMessagesBySenderIdAndReceiverId(@Param("userId") String userId, @Param("receiverId") String receiverId);
 }
 
